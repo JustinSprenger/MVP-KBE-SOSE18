44fc0c76f6d1f6234f5fbe319399d126632279fc
s0556255 <s0556255@saturn17-linux.wh.f4.htw-berlin.de>
2018-05-02 15:51:09 +0200

    asd

--- runMeRunner/src/main/java/de/htw/ai/kbe/runMeRunner/App.java
+++ runMeRunner/src/main/java/de/htw/ai/kbe/runMeRunner/App.java
@@ -1,7 +1,4 @@
 package de.htw.ai.kbe.runMeRunner;
-import java.io.IOException;
-
-import de.htw.ai.kbe.PropsFileUtil.*;;
 
 /**
  * Hello world!
@@ -11,18 +8,88 @@ public class App
 {
     public static void main( String[] args )
     {
-    	String cls;
-    	try {
-			cls = PropsFileUtil.readProperties(args[0]).getProperty("classToLoad");
-		} catch (PropsFileReadException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-    	cls = "de.htw.ai.kbe.MyClassWithRunMes";
-    	
-    	//method.invoke();
-    	System.out.println( "Hello World!" );
+    	if(args.length == 0) {
+    		RunMeLoader rml = new RunMeLoader();
+        	
+    		//"de.htw.ai.kbe.MyClassWithRunMes"
+    		if(rml.validInput("props.properties")) {
+    			rml.runMethods("");
+    		}
+    			
+    	}
+    	else {
+    		System.out.println("It should be run: \"java jar runMeRunner-1.0-jar-with-dependencies.jar –p propsFile –o runMeReport.txt\"");
+    	}
     }
-    
+ 	String cls;
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+//	try {
+//		if(!PropsFileUtil.readProperties(args[0]).getProperty("classToLoad").isEmpty()) {
+//			cls=PropsFileUtil.readProperties(args[0]).getProperty("classToLoad");
+//		}else {
+//			System.out.println("");
+//		}
+//	} catch (PropsFileReadException e) {
+//		e.printStackTrace();
+//	} catch (IOException e) {
+//		e.printStackTrace();
+//	}
+//	cls = "de.htw.ai.kbe.MyClassWithRunMes";
+//	if(cls!=null) {
+//		rml.load(cls);
+//	}else {
+//		System.out.println("In der Props ist kein Klassenname enthalten");
+//	}
+ 	
+ 	
 }
---
+++ runMeRunner/src/main/java/de/htw/ai/kbe/runMeRunner/RunMeLoader.java
@@ -0,0 +1,134 @@
+package de.htw.ai.kbe.runMeRunner;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+
+import de.htw.ai.kbe.PropsFileUtil.PropsFileReadException;
+import de.htw.ai.kbe.PropsFileUtil.PropsFileUtil;
+
+public class RunMeLoader {
+	Class<?> clas;
+	String cls;
+	public RunMeLoader() {
+		clas = null;
+	}
+	
+	private void load() {
+
+    	try {
+    		clas = Class.forName(cls);
+    	
+    	}catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+    
+	}
+
+	public boolean validInput(String input) {
+		boolean validInput = false; 
+		
+		try {
+			if(!PropsFileUtil.readProperties(input).getProperty("classToLoad").isEmpty()) {
+				validInput = true; 
+				cls=PropsFileUtil.readProperties(input).getProperty("classToLoad");
+			}else {
+				validInput = false; 
+				System.out.println("In der Props ist kein Klassenname enthalten");
+			}
+		} catch (PropsFileReadException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+					
+		return validInput;
+	}
+
+	public void runMethods(String outputFilename) {
+		this.load();
+		int methodCount = 0;
+		List<String> methodNamesWithRunMe = new ArrayList<String>();
+		List<String> methodNamesNotInvokable = new ArrayList<String>();
+		
+		
+		methodCount = (clas.getDeclaredMethods().length!= -1) ? clas.getDeclaredMethods().length : 0;
+		
+		for(final Method method : clas.getDeclaredMethods()) {
+		
+			if(method.isAnnotationPresent(RunMe.class)) {
+				
+				methodNamesWithRunMe.add(method.getName());
+				 if(!method.isAccessible())
+					 
+				try {
+					notInvokable = (boolean) method.invoke(method.getClass().newInstance());
+				} catch (IllegalAccessException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IllegalArgumentException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (InvocationTargetException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (InstantiationException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				if(!notInvokable)
+					methodNamesNotInvokable.add(method.getName());
+				
+			}
+		}
+
+		if(!outputFilename.isEmpty()) {
+			PrintStream output = null;
+			try {
+				output = new PrintStream(new FileOutputStream(outputFilename));
+				
+				output.printf("Anzahl der Methoden : %d%n", methodCount);
+								
+				for(int j =0; j < methodNamesWithRunMe.size();j++) {
+					output.printf("%n%nMethoden mit Anno%nMethod %d : %s%n", j, methodNamesWithRunMe.get(j));
+				}
+				
+				for(int j =0; j < methodNamesNotInvokable.size();j++) {
+					output.printf("%n%nMethoden mit Anno nicht invokabel%nMethod %d : %s%n", j, methodNamesNotInvokable.get(j));
+				}
+				
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			System.setOut(output);
+		}
+		else {
+			
+			
+			System.out.printf("Anzahl der Methoden - %d%n", methodCount);
+							
+			for(int j =0; j < methodNamesWithRunMe.size();j++) {
+				System.out.printf("%n%nMethoden mit Anno%nMethod  : %s%n", methodNamesWithRunMe.get(j));
+			}
+			
+			for(int j =0; j < methodNamesNotInvokable.size();j++) {
+				System.out.printf("%n%nMethoden mit Anno nicht invokabel%nMethod %d : %s%n", j, methodNamesNotInvokable.get(j));
+			}
+		}
+		
+	}
+
+	@Override
+	public String toString() {
+		return "";
+	}
+	
+	
+}
