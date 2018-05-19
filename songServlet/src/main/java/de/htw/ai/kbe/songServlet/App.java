b8b448d520216a53d6d3b1d5d8d2fcaf46331dfd
Justin <justin_sprenger@hotmail.de>
2018-05-19 14:40:05 +0200

    -init

---
+++ songServlet/src/main/java/de/htw/ai/kbe/songServlet/App.java
@@ -0,0 +1,10 @@
+package de.htw.ai.kbe.songServlet;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ songServlet/src/test/java/de/htw/ai/kbe/songServlet/AppTest.java
@@ -0,0 +1,10 @@
+package de.htw.ai.kbe.songServlet;
+
+public class AppTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
